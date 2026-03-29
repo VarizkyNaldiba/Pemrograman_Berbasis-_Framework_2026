63306525510bbad481254f52a5eec01cb2f3aa79
Zelectra <2341720243@student.polinema.ac.id>
2026-03-29 20:59:04 +0700

    Jobsheet 9

---
+++ my-app/src/pages/produk/server.tsx
@@ -0,0 +1,28 @@
+import TampilanProduk from "../views/produk";
+import { ProductType } from "../type/product.type";
+
+type PageProps = {
+    products: ProductType[];
+};
+
+const halamanProdukServer = ({ products }: PageProps) => {
+    return (
+        <div>
+            <h1>Halaman Produk Server</h1>
+            <TampilanProduk products={products} />
+        </div>
+    );
+};
+
+export default halamanProdukServer;
+
+export async function getServerSideProps(context: any) {
+    const baseUrl = `http://${context.req.headers.host}`;
+    const res = await fetch(`${baseUrl}/api/produk`);
+    const response = await res.json();
+    return {
+        props: {
+            products: Array.isArray(response?.data) ? response.data : [],
+        },
+    };
+}
